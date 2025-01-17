a4c79611bffcd6b25722f1ec28bc2cb7de8405e0
“Swapnadip <swapnadip123.saha@gmail.com>
2025-01-17 23:29:00 +0530

    17-01-2025 23:30

--- 2. Second APP Component Creation/src/App.jsx
+++ 2. Second APP Component Creation/src/App.jsx
@@ -1,11 +1,40 @@
+import randomImage from "./assets/react-core-concepts.png"
+const dynamicValue=["Fundamental","Crucial","Core"];
+
+
+export const userData = {
+  firstName: 'Maximilian', // feel free to replace the name value
+  lastName: 'Schwarzmüller', // feel free to replace the name value
+  title: 'Instructor', // feel free to replace the title value
+};
+
+export function User() {
+  const { firstName, lastName, title } = userData;
+
+  return (
+    <div id="user" data-testid="user">
+      <h2>
+        {firstName} {lastName}
+      </h2>
+      <p>{title}</p>
+    </div>
+  );
+}
+
+function genRandomInt(max){
+  return Math.floor(Math.random()*(max+1));
+}
 function Header(){
+  const description=dynamicValue[genRandomInt(2)];
     return(
         <header>
-          <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+          <img src={randomImage} alt="Stylized atom" />
           <h1>React Essentials</h1>
           <p>
-            Fundamental React concepts you will need for almost any app you are
+            {description} React concepts you will need for almost any app you are
             going to build!
+            
+            
           </p>
         </header>
     )
@@ -29,10 +58,13 @@ function App() {
         }
         <main>
           <h2>Time to get started!</h2>
+          <User />
         </main>
+       
       </div>
+      
     );
   }
   
   export default App;
-  
+
