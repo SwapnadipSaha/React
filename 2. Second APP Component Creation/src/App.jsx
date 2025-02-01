8bc802f9387283cffa8daff1439e06e70591268f
“Swapnadip <swapnadip123.saha@gmail.com>
2025-02-01 23:59:48 +0530

    02-02-2025 00:00

--- 2. Second APP Component Creation/src/App.jsx
+++ 2. Second APP Component Creation/src/App.jsx
@@ -1,92 +1,57 @@
-import randomImage from "./assets/react-core-concepts.png"
-import componentImage from "./assets/components.png"
+import reactImage from "./assets/react-core-concepts.png";
+import componentImage from "./assets/components.png";
 const dynamicValue=["Fundamental","Crucial","Core"];
-
-
-export const userData = {
-  firstName: 'Maximilian', // feel free to replace the name value
-  lastName: 'Schwarzmüller', // feel free to replace the name value
-  title: 'Instructor', // feel free to replace the title value
-};
-
-export function User() {
-  const { firstName, lastName, title } = userData;
-
-  return (
-    <div id="user" data-testid="user">
-      <h2>
-        {firstName} {lastName}
-      </h2>
-      <p>{title}</p>
-    </div>
-  );
-}
 
 function genRandomInt(max){
   return Math.floor(Math.random()*(max+1));
 }
-function Header(){
-  const description=dynamicValue[genRandomInt(2)];
-    return(
-        <header>
-          <img src={randomImage} alt="Stylized atom" />
-          <h1>React Essentials</h1>
-          <p>
-            {description} React concepts you will need for almost any app you are
-            going to build!
-            
-            
-          </p>
-        </header>
-    )
-}
-function CoreConcepts(props){
-  return(
-    <li>
-      <img src={props.image} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
+
+
+function Header()
+{
+  const randomValue=dynamicValue [genRandomInt(2)];
+  return (
+    <header>
+    <img src={reactImage} alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <p>
+      {randomValue} React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>
   )
 }
+function CoreConcept(props) {
+  return (
+  <li>
+  <img src={props.image} alt={props.title} />
+  <h3>{props.title}</h3>
+  <p>{props.description}</p>
+  </li>
+  );
+
+  }
 function App() {
     return (
-      <div>
-        {/* <header>
-          <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-          <h1>React Essentials</h1>
-          <p>
-            Fundamental React concepts you will need for almost any app you are
-            going to build!
-          </p>
-        </header> */
+      <div>        
+        <Header/> 
+          <main>
+            <section id="core-concepts">
+            <h2>Core Concepts</h2>
+            <ul>
+              <CoreConcept title="Components" description="The core UI Building Block" image={componentImage}></CoreConcept>
+              <CoreConcept></CoreConcept>
+              <CoreConcept></CoreConcept>
+              <CoreConcept></CoreConcept>
+            </ul>
 
-            // the header section will now be moved to new component or function.
-            // rule the function or component will be always initcap and the function name will be the tag here
-        <Header></Header>
-        }
-        <main>
-          <section id="core-concepts">
-          <ul>          
-            <CoreConcepts 
-              title="Components" 
-              description="The core UI"
-              image={componentImage}/>
-            <CoreConcepts/>
-            <CoreConcepts/>
-            <CoreConcepts/>
-          </ul>
-          </section>
-
-
-          <h2>Time to get started!</h2>
-          <User />
-        </main>
-       
+            </section>
+           
+          </main>          
       </div>
       
     );
   }
-  
+    
   export default App;
 
